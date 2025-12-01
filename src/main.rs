825aa8ca9f266ffa576dc9e6abef6f20f908d979
p1xelmind <trubichkinek@gmail.com>
2025-12-01 20:33:19 +1000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
-fn main(){
+#[tokio::main]
+async fn main(){
+    let app = Router::new()
+        .route("/", get(root));
 
+}
+
+async fn root() -> &'static str {
+    "Hello, world!"
 }
