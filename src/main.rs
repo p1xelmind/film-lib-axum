1ff163904131ba908a96b2d878c62b76c5eaf92b
p1xelmind <trubichkinek@gmail.com>
2025-12-02 21:17:38 +1000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main(){
     let app = Router::new()
         .route("/", get(root));
 
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{addr}");
+
+    let listener = TcpListener::bind(addr)
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
 
 async fn root() -> &'static str {
